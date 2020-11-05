de713d55ad419a90bed871e7645a8ae426761588
Pratik Karki <pratik.karki@ventur8.io>
2020-11-05 10:19:45 +0545

    Drop testing store for now in cljs and create a basic test for it

---
+++ test/fractl/test/core_test.cljs
@@ -0,0 +1,6 @@
+(ns fractl.test.core-test
+  "A basic cljs test."
+  (:require [cljs.test :refer-macros [deftest is]]))
+
+(deftest test-numbers
+  (is (= 1 1)))
--- test/fractl/test/test_runner.cljs
+++ test/fractl/test/test_runner.cljs
@@ -2,9 +2,10 @@
   "This ns should be used to run tests with `figwheel-main` test runner."
   (:require [cljs-test-display.core]
             [figwheel.main.testing :refer-macros [run-tests run-tests-async]]
-            [fractl.test.basic]))
+            ;; [fractl.test.basic]
+            [fractl.test.core-test]))
 
-(run-tests 'fractl.test.basic)
+(run-tests 'fractl.test.core-test)
 
 
 (defn -main [& args]
