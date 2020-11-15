d20b578ee239dab44bcc4c5cf830742838f35c06
Pratik Karki <pratik.karki@ventur8.io>
2020-11-15 16:22:50 +0545

    Use store protocol

--- src/fractl/sql_store.cljs
+++ src/fractl/sql_store.cljs
@@ -1,7 +1,9 @@
 (ns fractl.sql-store
-  (:require [fractl.store.sqljs-protocol :as p]
+  (:require [fractl.store.protocol :as p]
             [fractl.store.sqljs :as sq]
             [fractl.util :as u]))
+
+(def ^:private default-store (u/make-cell))
 
 (defn get-default-store []
   (u/safe-set-once
@@ -9,5 +11,11 @@
    #(let [store (sq/make)]
       store)))
 
+(def open-connection p/open-connection)
+(def close-connection p/close-connection)
 (def create-schema p/create-schema)
 (def drop-schema p/drop-schema)
+(def upsert-instance p/upsert-instance)
+(def delete-instance p/delete-instance)
+(def find-by-id p/find-by-id)
+(def find-by-query p/find-by-query)
