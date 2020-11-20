3044cb191818a3199cbbfb0dd452a8150b0fb018
Pratik Karki <pratik.karki@ventur8.io>
2020-11-20 13:46:05 +0545

    Sync upstream with downstream

--- src/fractl/store/sqljs.cljs
+++ src/fractl/store/sqljs.cljs
@@ -1,23 +1,27 @@
 (ns fractl.store.sqljs
   "The storage layer implementation for SQL.js database."
-  (:require [fractl.store.protocol :as p]
+  (:require [fractl.util :as u]
+            [fractl.store.protocol :as p]
             [fractl.store.sqljs-internal :as i]))
 
 (defn make []
-  (reify p/Store
-    (open-connection [store connection-info])
-    (close-connection [_])
-    (create-schema [_ component-name]
-      (i/create-schema component-name))
-    (drop-schema [_ component-name]
-      (i/drop-schema component-name))
-    (upsert-instance [_ entity-name instance]
-      (i/upsert-instance entity-name instance))
-    (delete-instance [_ entity-name instance]
-      (i/delete-instance entity-name instance))
-    (query-by-id [_ entity-name query ids]
-      (i/query-by-id entity-name query ids))
-    (do-query [_ query params]
-      (i/do-query query params))
-    (compile-query [_ query-pattern]
-      (i/compile-to-indexed-query query-pattern))))
+  (let [datasource (atom {})]
+    (reify p/Store
+      (open-connection [this connection-info]
+        true)
+      (close-connection [this]
+        true)
+      (create-schema [this component-name]
+        (i/create-schema component-name))
+      (drop-schema [this component-name]
+        (i/drop-schema component-name))
+      (upsert-instance [this entity-name instance]
+        (i/upsert-instance entity-name instance))
+      (delete-instance [this entity-name instance]
+        (i/delete-instance entity-name instance))
+      (query-by-id [this entity-name query ids]
+        (i/query-by-id entity-name query ids))
+      (do-query [this query params]
+        (i/do-query query params))
+      (compile-query [this query-pattern]
+        (i/compile-to-indexed-query query-pattern)))))
