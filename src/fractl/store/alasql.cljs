2cdd7506ac73b74274b588e5283b749c4438320b
Pratik Karki <pratik.karki@ventur8.io>
2020-11-27 22:34:22 +0545

    Rename files and remove files that aren't needed now.

--- src/fractl/store/alasql.cljs
+++ src/fractl/store/alasql.cljs
@@ -5,14 +5,46 @@
             ;[fractl.store.protocol :as p]
             [fractl.store.proj :as p]
             ;[fractl.store.sqljs-internal :as i]
-            [fractl.store.sql-internal :as i]))
+            [fractl.store.alasql-internal :as i]))
+
+(defn make []
+  (let [datasource (u/make-cell)]
+    (reify p/Store
+      (open-connection [store connection-info]
+        (let [connection-info (su/normalize-connection-info connection-info)]
+          (u/safe-set-once
+            datasource
+            (. alasql Database (:dbname connection-info)))
+          true))
+      (close-connection [_]
+        (try
+          (do (u/safe-set-result
+                datasource
+                (when @datasource
+                  (cp/close-pooled-datasource @datasource)
+                  nil))
+              true)
+          (catch Exception _ false)))
+      (create-schema [_ component-name]
+        (i/create-schema @datasource component-name))
+      (drop-schema [_ component-name]
+        (i/drop-schema @datasource component-name))
+      (upsert-instance [_ entity-name instance]
+        (i/upsert-instance @datasource entity-name instance))
+      (delete-instance [_ entity-name instance]
+        (i/delete-instance @datasource entity-name instance))
+      (query-by-id [_ entity-name query ids]
+        (i/query-by-id @datasource entity-name query ids))
+      (do-query [_ query params]
+        (i/do-query @datasource query params))
+      (compile-query [_ query-pattern]
+        (i/compile-to-indexed-query query-pattern)))))
 
 #_(defn make []
   (let [datasource (u/make-cell)]
     (reify p/Store
       (open-connection [_ connection-info]
-        (let [connection-info (su/normalize-connection-info connection-info)
-              set-f (partial u/safe-set-once datasource)]
+        (let [connection-info (su/normalize-connection-info connection-info)]
           (i/open-connection connection-info set-f)
           true))
       (close-connection [_]
@@ -38,6 +70,8 @@
         (i/do-query @datasource query params))
       (compile-query [_ query-pattern]
         (i/compile-to-indexed-query query-pattern)))))
+
+
 
 (defn make []
   (reify
