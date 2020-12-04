d25fd60a2cbfa3669118cf6529d99bec0dfeed96
Ranga Rao <ranga.rao@ventur8.io>
2020-12-04 12:35:20 -0800

    Revert "Cleanup - Phase 3 - cleanup of project.clj and commit of alasql as local jar dependency"
    This reverts commit 9ced4dc2f8e7ff07d021d282f148c718a9707ed3.

--- project.clj
+++ project.clj
@@ -19,18 +19,48 @@
   :main fractl.core
   :aot :all
 
-  :repositories {"local" "file:.m2/repositories"}
-
   :plugins [[lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]
             [lein-doo "0.1.10"]]
 
   :profiles {:dev {:dependencies [[com.bhauman/rebel-readline-cljs "0.1.4"]
                                   [com.bhauman/figwheel-main "0.2.12"]]
                    ;; setup target as a resource path
-                   :resource-paths ["target" "resources"]
+                   :resource-paths ["target" "resources" "node_modules"]
                    ;; set up an alias to invoke your figwheel build
                    :aliases  {"fig"       ["trampoline" "run" "-m" "figwheel.main"]
                               "fig:build" ["trampoline" "run" "-m" "figwheel.main" "-b" "dev" "-r"]
                               "fig:min"   ["run" "-m" "figwheel.main" "-O" "advanced" "-bo" "dev"]
                               "fig:test"  ["run" "-m" "figwheel.main" "-co" "test.cljs.edn" "-m" "fractl.test-runner"]}
-                   :clean-targets  ^{:protect false} ["target" "out"]}})
+                   :clean-targets  ^{:protect false} ["target" "out"]}}
+
+  :cljsbuild {:builds
+              [{:id "dev"
+                :source-paths ["src"]
+                :compiler  {:main                 fractl.core
+                            :target               :bundle
+                            :asset-path           "js/compiled/out"
+                            :output-to            "resources/public/js/compiled/out/index.js"
+                            :output-dir           "resources/public/js/compiled/out"
+                            :bundle-cmd           {:none    ["npx" "webpack" "--mode=development"]
+                                                   :default ["npx" "webpack"]}
+                            :source-map-timestamp true
+                            ;; To console.log CLJS data-structures make sure you enable devtools in Chrome
+                            ;; https://github.com/binaryage/cljs-devtools
+                            :preloads             [devtools.preload]}}
+               {:id "test"
+                :compiler
+                     {:asset-path    "target/test"
+                      :main          fractl.test
+                      :optimizations :none
+                      :output-dir    "target/test"
+                      :output-to     "target/test.js"}
+                :source-paths ["src" "test"]}
+               ;; This next build is a compressed minified build for
+               ;; production. You can build this with:
+               ;; lein cljsbuild once min
+               {:id "min"
+                :source-paths ["src"]
+                :compiler  {:output-to     "resources/public/js/compiled/fractl.js"
+                            :main          fractl.core
+                            :optimizations :advanced
+                            :pretty-print  false}}]})
