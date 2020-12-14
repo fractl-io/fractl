3d7d48c95a05d259476309fbbe53b68f9040d011
Pratik Karki <pratik.karki@ventur8.io>
2020-12-14 14:04:17 +0545

    Fix port no. in script

--- sample/httpreq.sh
+++ sample/httpreq.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl --header "Content-Type: application/json" --request POST --data '{"Sample.Simple/Upsert_E1": {"Instance": {"Sample.Simple/E1": {"X":100}}}}' http://localhost:8000/_e/Sample.Simple/Upsert_E1
+curl --header "Content-Type: application/json" --request POST --data '{"Sample.Simple/Upsert_E1": {"Instance": {"Sample.Simple/E1": {"X":100}}}}' http://localhost:8080/_e/Sample.Simple/Upsert_E1
