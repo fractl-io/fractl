76e0a94b17db4647ad72bfb3ab542c793e77cbd7
Ranga Rao <ranga.rao@ventur8.io>
2020-12-24 16:10:22 -0800

    Bumping version number to v0.1.1 - prep for releasing delete pattern functionality

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fractl-io/fractl "0.1.0"
+(defproject fractl-io/fractl "0.1.1"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "1.10.773"]
                  [org.clojure/tools.cli "1.0.194"]
