6b8addcd9368b8c2c2a39fb62e7256ddfc8e1c08
Ranga Rao <ranga.rao@ventur8.io>
2021-02-10 10:44:05 -0800

    Bump version to v0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fractl-io/fractl "0.1.1"
+(defproject fractl-io/fractl "0.1.2"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "1.10.773"]
                  [org.clojure/tools.cli "1.0.194"]
