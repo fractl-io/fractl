99b9bee6ab696bd72841320676447f6d5513feed
Pratik Karki <pratik.karki@ventur8.io>
2021-05-17 17:14:54 +0545

    Resolver implementation of sms
    Signed-off-by: Pratik Karki <pratik.karki@ventur8.io>

--- src/fractl/resolver/sms.clj
+++ src/fractl/resolver/sms.clj
@@ -1,1 +1,18 @@
-(ns fractl.resolver.sms)
+(ns fractl.resolver.sms
+  (:require [fractl.resolver.core :as r]
+            [fractl.sms :as sms]
+            [fractl.util :as u]))
+
+(defn- sms-eval [inst]
+  (let [body (:Body inst)
+        from (str (u/getenv "TWILIO_PHONE_NUMBER"))
+        to (:To inst)]
+    (sms/send-sms {:From from
+                   :To to
+                   :Body body})))
+
+(def ^:private resolver-fns
+  {:eval {:handler sms-eval}})
+
+(defn make [resolver-name config]
+  (r/make-resolver resolver-name resolver-fns))
