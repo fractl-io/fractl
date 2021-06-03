9baa6131eb6eba041dfdb5bd0c184b898920e886
Pratik Karki <73872198+kitrerp@users.noreply.github.com>
2021-06-03 10:39:39 +0545

    Refactor out query by id as they are jdbc specific (#227)
    Signed-off-by: Pratik Karki <pratik.karki@ventur8.io>

--- src/fractl/store/h2_internal.clj
+++ src/fractl/store/h2_internal.clj
@@ -1,6 +1,5 @@
 (ns fractl.store.h2-internal
-  (:require [next.jdbc :as jdbc]
-            [next.jdbc.prepare :as jdbcp])
+  (:require [next.jdbc :as jdbc])
   (:import [java.sql PreparedStatement]))
 
 (defn upsert-index-statement [conn table-name _ id attrval]
@@ -12,9 +11,3 @@
   (let [sql (str "MERGE INTO " table-name " KEY (id) VALUES (?, ?)")
         ^PreparedStatement pstmt (jdbc/prepare conn [sql])]
     [pstmt [id obj]]))
-
-(defn query-by-id-statement [conn query-sql id]
-  (let [^PreparedStatement pstmt (jdbc/prepare conn [query-sql])]
-    (.setString pstmt 1 (str id))
-    [pstmt nil]))
-
