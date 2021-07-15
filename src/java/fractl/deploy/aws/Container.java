6d3a71d32d41f1c8548637df85baad5c30641b81
Vijay Mathew <vijay.mathew@ventur8.io>
2021-07-15 15:55:50 +0530

    create repository

--- src/java/fractl/deploy/aws/Container.java
+++ src/java/fractl/deploy/aws/Container.java
@@ -1,13 +1,26 @@
 package fractl.deploy.aws;
 
+import java.util.concurrent.CompletableFuture;
+import clojure.lang.IFn;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.ecr.EcrAsyncClient;
+import software.amazon.awssdk.services.ecr.model.CreateRepositoryRequest;
+import software.amazon.awssdk.services.ecr.model.CreateRepositoryResponse;
 
 public final class Container {
     public static EcrAsyncClient buildClient(Region region) {
-	EcrAsyncClient client = EcrAsyncClient.builder()
+	return EcrAsyncClient.builder()
 	    .region(region)
 	    .build();
-	return client;
+    }
+
+    public static void createRepository(EcrAsyncClient client, String reponame, IFn callback) {
+	CreateRepositoryRequest req = CreateRepositoryRequest.builder()
+	    .repositoryName(reponame)
+	    .build();
+	CompletableFuture<CreateRepositoryResponse> future = client.createRepository(req);
+	future.whenComplete((reponse, err) -> {
+		callback.invoke(reponse, err);
+	    });
     }
 }
