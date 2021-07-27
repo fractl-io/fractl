5d5df722dcce3824f781012950dd9dcd20a59a69
Pratik Karki <pratik.karki@ventur8.io>
2021-07-27 15:38:31 +0545

    Explicitly open reagent store during eval
    Signed-off-by: Pratik Karki <pratik.karki@ventur8.io>

--- test/fractl/test/reagent.cljs
+++ test/fractl/test/reagent.cljs
@@ -9,8 +9,6 @@
             [fractl.resolver.registry :as rg]
             [fractl.test.util :as tu :refer-macros [defcomponent]]))
 
-(def store (store/open-reagent-store nil))
-
 (defn- test-resolver [install-resolver resolver-name path]
   (let [r (r/make-resolver resolver-name {:upsert {:handler identity}
                                           :delete {:handler (fn [x] x)}})]
@@ -22,50 +20,13 @@
 (defn- persisted? [comp-name entity-instance]
   (let [id (:Id entity-instance)
         evt (cn/make-instance (keyword (str (name comp-name) "/Lookup_E")) {:Id id})
-        result (e/eval-all-dataflows evt store nil)
+        result (e/eval-all-dataflows evt (store/open-reagent-store nil) nil)
         r (first result)]
     (when-not (= :not-found (:status r))
       (let [e (ffirst (:result r))]
         (cn/same-instance? entity-instance e)))))
 
-; Enable this when not running alasql
-(deftest store-test
-  (defcomponent :ST
-    (entity {:ST/E {:X :Kernel/Int}})
-
-    (event {:ST/NewE {:X :Kernel/Int}})
-
-    (dataflow :ST/NewE
-              {:ST/E {:X :ST/NewE.X}}
-              :ST/E.X))
-
-  (let [e (cn/make-instance :ST/E {:X 10})
-        evt (cn/make-instance :ST/Upsert_E {:Instance e})
-        result (tu/fresult (e/eval-all-dataflows evt store nil))
-        e01 (ffirst result)]
-    (is (cn/instance-of? :ST/E e01))
-    (is (nil? (second result)))
-    (is (persisted? :ST e01)))
-  (compose-test-resolver :TestResolver01 :ST/E)
-  (let [e (cn/make-instance :ST/E {:X 10})
-        evt (cn/make-instance :ST/Upsert_E {:Instance e})
-        result (tu/fresult (e/eval-all-dataflows evt store nil))
-        e01 (ffirst result)
-        r (ffirst (second result))]
-    (is (cn/instance-of? :ST/E e01))
-    (is (persisted? :ST e01))
-    (is (cn/instance-of? :ST/E r))
-    (is (= e01 r))
-    (let [evt (cn/make-instance :ST/NewE {:X 100})
-          result (tu/fresult (e/eval-all-dataflows evt store nil))]
-      (is (= :cursor (first result)))
-      (let [cursor (second result)]
-        (is (vector? cursor))
-        (is (= [:ST :E] (first cursor)))
-        (is (= :X (last cursor)))))))
-
-; Test for alasql reagent
-#_(deftest store-test-alasql
+(deftest store-test-alasql
   (defcomponent :ST
                 (entity {:ST/E {:X :Kernel/Int}})
 
@@ -77,7 +38,7 @@
 
   (let [e (cn/make-instance :ST/E {:X 10})
         evt (cn/make-instance :ST/Upsert_E {:Instance e})
-        result (tu/fresult (e/eval-all-dataflows evt store nil))
+        result (tu/fresult (e/eval-all-dataflows evt (store/open-reagent-store nil) nil))
         e01 (ffirst result)]
     (is (cn/instance-of? :ST/E e01))
     (is (nil? (second result)))
@@ -85,7 +46,7 @@
   (compose-test-resolver :TestResolver01 :ST/E)
   (let [e (cn/make-instance :ST/E {:X 10})
         evt (cn/make-instance :ST/Upsert_E {:Instance e})
-        result (tu/fresult (e/eval-all-dataflows evt store nil))
+        result (tu/fresult (e/eval-all-dataflows evt (store/open-reagent-store nil) nil))
         e01 (ffirst result)
         r (ffirst (second result))]
     (is (cn/instance-of? :ST/E e01))
@@ -93,6 +54,6 @@
     (is (cn/instance-of? :ST/E r))
     (is (= e01 r))
     (let [evt (cn/make-instance :ST/NewE {:X 100})
-          result (tu/fresult (e/eval-all-dataflows evt store nil))
+          result (tu/fresult (e/eval-all-dataflows evt (store/open-reagent-store nil) nil))
           cursor result]
       (is (= 100 cursor)))))
