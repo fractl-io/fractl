bd113995692ae1fdead370fad7c6107256c2b36f
vmatv8 <73871437+vmatv8@users.noreply.github.com>
2021-08-11 12:43:27 +0530

    Issue 321 fqnames (#326)
    * rename in expressions
    
    * rewrite query values
    
    * automated test

--- src/fractl/lang/loader.clj
+++ src/fractl/lang/loader.clj
@@ -6,26 +6,48 @@
             [fractl.component :as cn])
   (:import [java.io FileInputStream InputStreamReader PushbackReader]))
 
+(defn- record-name [obj]
+  (if (keyword? obj)
+    obj
+    (first (keys obj))))
+
+(defn- fetch-declared-names [script-file]
+  (loop [exps (read-string (str "(do" (slurp script-file) ")"))
+         result {}]
+    (if-let [exp (first exps)]
+      (recur
+       (rest exps)
+       (if (seqable? exp)
+         (case (first exp)
+           component
+           (assoc result :component (second exp))
+
+           (entity record event)
+           (assoc
+            result :records
+            (conj
+             (:records result)
+             (record-name (second exp))))
+
+           result)))
+      result)))
+
 (defn read-expressions
   "Read expressions in sequence from a fractl component file. Each expression read
    is preprocessed to add component-name prefixes to names. Then the expression is evaluated.
    Return a list with the results of evaluations."
-  [file-name]
-  (let [reader (PushbackReader.
-                (InputStreamReader.
-                 (FileInputStream. file-name)))
-        rdf #(read reader nil :done)]
-    (loop [exp (rdf), exps nil]
-      (if (= exp :done)
-        exps
-        (recur (rdf) (conj exps (eval (nu/fully-qualified-names exp))))))))
-
-(defn- maybe-fetch-component-name [file-name]
-  (loop [exps (read-string (str "(do" (slurp file-name) ")"))]
-    (when-let [exp (first exps)]
-      (if (and (seqable? exp) (= 'component (first exp)))
-        (second exp)
-        (recur (rest exps))))))
+  ([file-name declared-names]
+   (let [reader (PushbackReader.
+                 (InputStreamReader.
+                  (FileInputStream. file-name)))
+         rdf #(read reader nil :done)
+         fqn (partial nu/fully-qualified-names declared-names)]
+     (loop [exp (rdf), exps nil]
+       (if (= exp :done)
+         exps
+         (recur (rdf) (conj exps (eval (fqn exp))))))))
+  ([file-name]
+   (read-expressions file-name (fetch-declared-names file-name))))
 
 (defn load-script
   "Load, complile and intern the component from a script file."
@@ -35,11 +57,12 @@
         (if (and component-root-path (not (.startsWith file-name component-root-path)))
           (str component-root-path u/path-sep file-name)
           file-name)
-        component-name (maybe-fetch-component-name full-file-name)]
+        names (fetch-declared-names full-file-name)
+        component-name (:component names)]
     (when component-name
       (cn/remove-component component-name))
     (binding [*ns* *ns*]
-      (read-expressions full-file-name))
+      (read-expressions full-file-name names))
     (when (and component-name (cn/component-exists? component-name))
       component-name)))
 
