9e2818244bda18292337a4513d2fc86b8528583f
Anand <anand.pillai@ventur8.io>
2021-12-16 20:18:04 +0530

    ref issue #404 - Scope is a string not list of strings

--- src/java/fractl/auth/auth0/Auth0AuthUtil.java
+++ src/java/fractl/auth/auth0/Auth0AuthUtil.java
@@ -2,18 +2,18 @@ package fractl.auth.auth0;
 
 import com.auth0.client.auth.AuthAPI;
 import com.auth0.client.auth.AuthorizeUrlBuilder;
-import java.util.Arrays;
 
 public class Auth0AuthUtil {
 
     public static String authorizeUrl(String clientId, String clientSecret, String authDomain,
-                                      String authCallbackUrl, String[] scope) {
+                                      String authCallbackUrl, String scope) {
         
         AuthAPI auth = new AuthAPI(authDomain, clientId, clientSecret);
         auth.setLoggingEnabled(true);
 
-        String authUrl = auth.authorizeUrl(authCallbackUrl).withScope(Arrays.toString(scope)).build();
-
+        String authUrl = auth.authorizeUrl(authCallbackUrl).withScope(scope).build();
+		//		System.out.println("Authorize URL => " + authUrl);
         return authUrl;
     }
+
 }
