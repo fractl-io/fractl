5a3f8d984c6a7998e5935a022ea681140ed4e821
Anand <anand.pillai@ventur8.io>
2021-12-20 13:29:57 +0530

    ref issue #414 - Auth0 wrapper to login using DB username/password

--- src/java/fractl/auth/auth0/Auth0AuthUtil.java
+++ src/java/fractl/auth/auth0/Auth0AuthUtil.java
@@ -2,6 +2,8 @@ package fractl.auth.auth0;
 
 import com.auth0.client.auth.AuthAPI;
 import com.auth0.client.auth.AuthorizeUrlBuilder;
+import com.auth0.json.auth.TokenHolder;
+import com.auth0.exception.Auth0Exception;
 
 public class Auth0AuthUtil {
 
@@ -15,5 +17,20 @@ public class Auth0AuthUtil {
 
         return authUrl;
     }
+
+	public static TokenHolder PasswordLogin(String clientId, String clientSecret, String authDomain,
+											String userName, String password, String scope) {
+
+        AuthAPI auth = new AuthAPI(authDomain, clientId, clientSecret);
+        auth.setLoggingEnabled(true);
+
+		try {
+			TokenHolder result = auth.login(userName, password.toCharArray()).setScope(scope).execute();
+			return result;
+		} catch (Auth0Exception ex) {
+			ex.printStackTrace();
+			return null;
+		}
+	}
 
 }
