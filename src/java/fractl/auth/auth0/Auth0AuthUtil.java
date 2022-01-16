1d0879743158f36f65b70e2a7749058563a6b323
Anand <anand.pillai@ventur8.io>
2022-01-16 11:11:43 +0530

    ref issue #422 - Management API + user deletetion end-point

--- src/java/fractl/auth/auth0/Auth0AuthUtil.java
+++ src/java/fractl/auth/auth0/Auth0AuthUtil.java
@@ -1,10 +1,12 @@
 package fractl.auth.auth0;
 
 import com.auth0.client.auth.AuthAPI;
+import com.auth0.client.mgmt.ManagementAPI;
 import com.auth0.client.auth.AuthorizeUrlBuilder;
 import com.auth0.json.auth.TokenHolder;
 import com.auth0.json.auth.CreatedUser;
 import com.auth0.exception.Auth0Exception;
+import com.auth0.exception.APIException;
 import com.auth0.json.auth.UserInfo;
 import java.util.Map;
 
@@ -15,6 +17,13 @@ public class Auth0AuthUtil {
 	public static AuthAPI createAuthAPI(String clientId, String clientSecret, String authDomain, boolean loggingEnabled) {
         AuthAPI api = new AuthAPI(authDomain, clientId, clientSecret);
         api.setLoggingEnabled(loggingEnabled);
+
+		return api;
+	}
+
+	public static ManagementAPI createMgmtAPI(String authDomain, String accessToken, boolean loggingEnabled) {
+		ManagementAPI api = new ManagementAPI(authDomain, accessToken);
+		api.setLoggingEnabled(loggingEnabled);
 
 		return api;
 	}
@@ -63,6 +72,18 @@ public class Auth0AuthUtil {
 			ex.printStackTrace();
 			return null;
 		}		
-	}	
+	}
+
+	public static Void deleteUser(ManagementAPI api, String userId) {
+		try {
+			return api.users().delete(userId).execute();
+		} catch (APIException ex) {
+			ex.printStackTrace();
+			return null;			
+		} catch (Auth0Exception ex) {
+			ex.printStackTrace();
+			return null;
+		}					
+	}
 
 }
