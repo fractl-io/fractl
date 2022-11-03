1f3142f1cf75529437740565f82ef2530d190597
Vijay Mathew <vijay.mathew@ventur8.io>
2022-11-03 14:55:06 +0530

    move loader to tools

--- sample/dependencies/billing/src/billing/core.clj
+++ sample/dependencies/billing/src/billing/core.clj
@@ -1,6 +1,6 @@
 (ns billing.core
   (:require [fractl.core :as f]
             [banking.core]
-            [billing.model.billing.core]))
+            [billing.model.model]))
 
 (defn -main [& args] (apply f/-main args))
