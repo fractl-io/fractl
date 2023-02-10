198a7e581ae0376caab3becb6f39866ff6a22a97
Ranga Rao <ranga@fractl.io>
2023-02-10 13:09:14 -0800

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fractl-io/fractl "0.2.4"
+(defproject fractl-io/fractl "0.3.0"
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/clojurescript "1.10.896"
                   :exclusions [com.google.code.findbugs/jsr305]]
