53e48fee479a78bfb364e95bdf4f2e650a3d2645
Vijay Mathew <vijay.mathew@ventur8.io>
2023-07-10 17:10:35 +0530

    system-paths only on jvm

--- src/fractl/lang/tools/util.clj
+++ src/fractl/lang/tools/util.clj
@@ -1,7 +1,8 @@
 (ns fractl.lang.tools.util
   (:require [clojure.string :as s]))  
 
-(defn get-system-model-paths []
-  (if-let [paths (System/getenv "FRACTL_MODEL_PATHS")]
-    (s/split paths #":")
-    ["."]))
+#?(:clj
+   (defn get-system-model-paths []
+     (if-let [paths (System/getenv "FRACTL_MODEL_PATHS")]
+       (s/split paths #":")
+       ["."])))
