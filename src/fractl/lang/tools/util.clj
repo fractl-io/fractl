a7eb68ce3246296cb682f504763ec228efc46528
vijayfractl <73871437+vijayfractl@users.noreply.github.com>
2023-07-12 08:49:34 +0530

    Loader support in the frontend  (#949)
    * loader is accesible from cljs
    
    * loader for cljs
    
    * make name-util available for cljs
    
    * system-paths only on jvm
    
    * make tools/util available for cljs
    
    * fix name-resolution and defn-eval
    
    * throw exception on intern-failure
    
    * fix cljs support errors
    
    ---------
    
    Co-authored-by: Vijay Mathew <vijay.mathew@ventur8.io>

--- src/fractl/lang/tools/util.clj
+++
@@ -1,7 +0,0 @@
-(ns fractl.lang.tools.util
-  (:require [clojure.string :as s]))  
-
-(defn get-system-model-paths []
-  (if-let [paths (System/getenv "FRACTL_MODEL_PATHS")]
-    (s/split paths #":")
-    ["."]))
