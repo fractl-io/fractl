878711f11538f15f9caf10806c344f7d0b405db0
Pratik Karki <pratik@fractl.io>
2024-04-29 09:41:49 +0545

    Update import order and add dependency fetching
    A new function 'get-dependencies' has been added in package.clj file.
    It fetches project dependencies from the model provided. The
    'project-spec' function has been modified to include these dependencies.
    
    Signed-off-by: Pratik Karki <pratik@fractl.io>

--- src/fractl/package.clj
+++ src/fractl/package.clj
@@ -8,16 +8,28 @@
 (def ^:private path-split-pat (re-pattern (Pattern/quote u/path-sep)))
 (def ^:private model-resource-root "app")
 
+(defn get-dependencies [m]
+  (let [dep-map (first (:dependencies m))]
+    (when (:type dep-map)
+      (cond
+        (= :lein (:type dep-map))
+        (let [source-map (:source dep-map)]
+          (when (= :clojars (:type source-map))
+            [(symbol (str (:org source-map) "/" (:package source-map))) (:version dep-map)]))))))
+
 (defn- project-spec [project-name model]
   `(~(symbol "defproject")
-    ~(symbol project-name)
-    ~(or (:version model)
-         "0.1-SNAPSHOT")
+     ~(symbol project-name)
+     ~(or (:version model)
+          "0.1-SNAPSHOT")
 
-    :main ~(symbol "fractl.core")
+     :main ~(symbol "fractl.core")
 
-    :repositories [["public-github" {:url "git://github.com"}]
-                   ["private-github" {:url "git://github.com" :protocol :ssh}]]))
+     :repositories [["public-github" {:url "git://github.com"}]
+                    ["private-github" {:url "git://github.com" :protocol :ssh}]]
+
+     ~@(when-let [deps (get-dependencies model)]
+         [:dependencies deps])))
 
 (defn- fetch-app-config [model-root-dir launch-config]
   (let [^File file (File. (str model-root-dir u/path-sep "config.edn"))]
