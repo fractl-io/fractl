7b3984b05776b847c9c1c18e4bd69a78acce5d15
Hasnain Naeem <hasnainnaeem@outlook.com>
2024-05-09 11:31:22 +0500

    addressed review comments

---
+++ src/fractl/lang/tools/nrepl/core.clj
@@ -0,0 +1,33 @@
+(ns fractl.lang.tools.nrepl.core
+  (:require
+    [fractl.component :as cn]
+    [fractl.lang :as ln]
+    [fractl.lang.tools.repl :as repl]
+    [fractl.lang.tools.replcmds :as replcmds]
+    [fractl.util.core :as uc]))
+
+(defonce nrepl-eval-init (atom nil))
+
+(defn initialize-nrepl-environment [model-name store evaluator]
+  "Initializes the REPL environment for a given model name."
+  (let [model-name (or model-name (repl/infer-model-name))
+        current-cn (cn/get-current-component)
+        decl-names (cn/declared-names current-cn)]
+    (when decl-names
+      (repl/set-declared-names! current-cn decl-names))
+    (use '[fractl.lang])
+    (use '[fractl.lang.tools.replcmds])
+    (ln/component repl/repl-component)
+    (let [cn (if (= model-name :fractl)
+               repl/repl-component
+               current-cn)]
+      (replcmds/switch cn))
+    (partial repl/repl-eval store (atom nil) evaluator)))
+
+(defn init-repl-eval-func [model-name options]
+  (uc/force-call-after-load-model
+    model-name
+    (fn []
+      (let [model-info (uc/read-model-and-config options)
+            [[ev store] _] (uc/prepare-repl-runtime model-info)]
+        (reset! nrepl-eval-init (initialize-nrepl-environment model-name store ev))))))
