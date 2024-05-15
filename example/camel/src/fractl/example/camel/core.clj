144c023d6ad5fe3fabeea882b2b097dd02875ea0
Vijay Mathew <73871437+vijayfractl@users.noreply.github.com>
2024-05-15 12:18:24 +0530

    factor-out code camel dependencies (#1333)

--- example/camel/src/fractl/example/camel/core.clj
+++
@@ -1,8 +0,0 @@
-(ns fractl.example.camel.core
-  (:require [fractl.core :as fractl]
-            [fractl.resolver.camel :as cr]
-            [fractl.example.camel.sfdc-resolver :as sfr]))
-
-(defn -main [& args]
-  (cr/register-component :salesforce sfr/make-component)
-  (fractl/run-script args))
