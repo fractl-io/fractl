f59d68f50eddb3b0364f5bfc3224d0778ffd3a95
Pratik Karki <pratik@fractl.io>
2024-06-12 12:25:05 +0545

    Update project version to 0.5.2
    Signed-off-by: Pratik Karki <pratik@fractl.io>

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.fractl-io/fractl "0.5.1"
+(defproject com.github.fractl-io/fractl "0.5.2"
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/clojurescript "1.10.896"
                   :exclusions [com.google.code.findbugs/jsr305]]
