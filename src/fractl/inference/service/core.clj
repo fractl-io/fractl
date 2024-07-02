1b1e428641ed534446336533649b1e5f23767e6a
Vijay Mathew <vijay@fractl.io>
2024-07-02 15:01:02 +0530

    init inference service

--- src/fractl/inference/service/core.clj
+++ src/fractl/inference/service/core.clj
@@ -1,7 +1,5 @@
 (ns fractl.inference.service.core
-  (:require [fractl.util.runtime :as fur]
-            fractl.inference.service.model                  ; force-load component
+  (:require fractl.inference.service.model ; force-load component
             [fractl.inference.service.resolver :as api-resolver]))
 
-(defn init []
-  (fur/set-on-init! #(do (api-resolver/register-resolver))))
+(defn init [] (api-resolver/register-resolver))
