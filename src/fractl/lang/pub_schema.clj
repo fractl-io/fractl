f32761c8c76b778cf710b3948a403403fe973bc7
Vijay Mathew <vijay@fractl.io>
2024-07-29 16:18:58 +0530

    ignore config in publish-schema

--- src/fractl/lang/pub_schema.clj
+++ src/fractl/lang/pub_schema.clj
@@ -4,7 +4,15 @@
             [fractl.global-state :as gs]
             [fractl.inference.embeddings :as e]))
 
-(defn publish-schema? [] (:publish-schema (gs/get-app-config)))
+(defn publish-schema? []
+  #_(:publish-schema (gs/get-app-config))
+  ;; NOTE: while running the app as a standalone jar
+  ;; the model is loaded before the config. So another
+  ;; option, like a system.property, has to be used.
+  ;; As we are using a local queue, publishing the schema
+  ;; itself will not throw an error, so setting this to
+  ;; always return `true`.
+  true)
 
 (defn- preproc-definition [d]
   (let [d (w/prewalk #(if (fn? %) :fn %) d)]
